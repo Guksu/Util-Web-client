7820da02d9516d7a2a558f6f72532192f47e781c
Guksu <yung3152@daum.net>
2022-01-20 14:11:05 +0900

    Profile 반응형 추가

--- src/components/ProfileComponents/ProfileLayOut.tsx
+++ src/components/ProfileComponents/ProfileLayOut.tsx
@@ -11,6 +11,18 @@ const LayOutDiv = styled.div`
   display: grid;
   grid-template-columns: 30% 70%;
   margin: 8% 20%;
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+  }
+  @media (max-width: 425px) {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    font-size: 15px;
+    margin: 8% 10%;
+  }
 `;
 
 const ContentName = styled.span`
@@ -25,13 +37,31 @@ const ContentDiv = styled.div`
   justify-content: space-between;
   border-radius: 25px;
   background-color: ${(props) => props.theme.backgroundColor};
+  @media (max-width: 425px) {
+    width: 300px;
+  }
+  @media (max-width: 375px) {
+    width: 280px;
+    font-size: 15px;
+  }
+  @media (max-width: 320px) {
+    width: 250px;
+    font-size: 15px;
+  }
 `;
 
 const LayOutBtn = styled.button`
   width: 6vw;
-  height: 3vh;
-  min-width: 70px;
-  min-height: 40px;
+  height: 5vh;
+  @media (max-width: 1024px) {
+    width: 120px;
+    height: 30px;
+  }
+  @media (max-width: 375px) {
+    width: 70px;
+    height: 40px;
+    font-size: 12px;
+  }
 `;
 
 function ProfileLayOut({ divName, btnName, divContent, btnOnclick }: Props) {
